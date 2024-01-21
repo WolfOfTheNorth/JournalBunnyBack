e4b1f487fe9cca85659cd15de177b3241312d7a4
Dave Bhardwaj <davebhardwaj@gmail.com>
2024-01-21 16:19:10 -0500

    Initial API

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+# journal/views.py
+from rest_framework import viewsets
+from .models import JournalEntry
+from .serializers import JournalEntrySerializer
+
+class JournalEntryViewSet(viewsets.ModelViewSet):
+    queryset = JournalEntry.objects.all()
+    serializer_class = JournalEntrySerializer
